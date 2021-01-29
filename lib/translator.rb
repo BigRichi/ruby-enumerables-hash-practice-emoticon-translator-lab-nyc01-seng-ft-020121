979919d538e49f0c5bdc9ff5e4d95cc9ff64fa47
Richard Adule <big.richi@outlook.com>
2021-01-29 15:24:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,22 +2,15 @@
 require 'pry'
 require 'yaml'
 
-# emot = YAML.load_file("lib/emoticons.yml").each_with_object({}) do |(symb,pics), final_array|
-#     final_array[symb] = {:english => pics[0] , :japanese => pics[-1]}
-# end 
-
-# puts emot
-
 def load_library(emot)
   YAML.load_file(emot).each_with_object({}) do |(symb,pics), final_array|
     final_array[symb] = {:english => pics[0] , :japanese => pics[-1]}
   end
 end
 
+def get_english_meaning
+  # code goes here
+end
 def get_japanese_emoticon(file_path, emoticon)
   binding.pry
 end
-
-# def get_english_meaning
-#   # code goes here
-# end

c2914c92e0524428c3a4d3232d8572a642347640
Richard Adule <big.richi@outlook.com>
2021-01-29 14:54:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,20 @@
 # require modules here
+require 'yaml'
+
+emot = YAML.load_file("lib/emoticons.yml").each_with_object({}) do |(symb,pics), final_array|
+    final_array[symb.to_sym] = {:english => pics[0] , :japanese => pics[-1]}
+end 
+
+p emot[:angel][:english]
 
 def load_library
-  # code goes here
+  emot = YAML.load_file("lib/emoticons.yml").each_with_object({}) do |(symb,pics), final_array|
+    final_array[symb.to_sym] = {:english => pics[0] , :japanese => pics[-1]}
+  end
+  return emot
 end
+
+# p load_library
 
 def get_japanese_emoticon
   # code goes here

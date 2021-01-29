206c609ee766846ca60c35d7be11b8cad4258d2b
Richard Adule <big.richi@outlook.com>
2021-01-29 15:15:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,6 @@ def load_library(emot)
   YAML.load_file(emot).each_with_object({}) do |(symb,pics), final_array|
     final_array[symb] = {:english => pics[0] , :japanese => pics[-1]}
   end
-  binding.pry
 end
 
 # # p load_library

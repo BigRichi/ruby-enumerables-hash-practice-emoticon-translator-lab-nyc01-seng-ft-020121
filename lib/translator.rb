422b945e71d0ca05c8f58d10729fbef396ded795
Richard Adule <big.richi@outlook.com>
2021-01-29 15:14:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,10 +8,11 @@ require 'yaml'
 
 # puts emot
 
-def load_library
-  emot = YAML.load_file("lib/emoticons.yml").each_with_object({}) do |(symb,pics), final_array|
-    final_array[symb] = {:english => pics[0] , :japanese => pics[-1]}
-  end
+def load_library(emot)
+  YAML.load_file(emot)
+  # .each_with_object({}) do |(symb,pics), final_array|
+  #   final_array[symb] = {:english => pics[0] , :japanese => pics[-1]}
+  # end
   binding.pry
 end
 
